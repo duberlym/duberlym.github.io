f174df12a22a52039c172f29e70dde94f965c2bb
duberly2004 <ivanmondragonmanchay@gmail.com>
2024-10-27 18:04:52 -0500

    📝 Update [astro.config.mjs]: base

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [tailwind()],
   site:'https://duberlym.github.io',
-  base: 'duberlym.github.io'
+  base: ''
 });
