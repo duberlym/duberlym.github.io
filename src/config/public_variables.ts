853b207c12f097fee71783bf136401fd0039da11
Duberly <108305203+jackmmd@users.noreply.github.com>
2024-10-30 16:46:57 -0500

    Update public_variables.ts

--- src/config/public_variables.ts
+++ src/config/public_variables.ts
@@ -1,5 +1,5 @@
 
 // export const BASE_URL = "https://duberlym.github.io"
-export const BASE_URL = import.meta.env.PUBLIC_BASE_URL || "https://duberlym.github.io"
+export const BASE_URL = import.meta.env.PUBLIC_BASE_URL || "https://jackmmd.github.io"
 
   
